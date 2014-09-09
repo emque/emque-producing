059b1151ef4d498dec1f7b2f9c25111ac7f7b914
Ryan Williams <ryan.williams@teamsnap.com>
2014-09-09 04:59:29 +0000

    Bringing in kafka spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,22 @@
-require 'emque-producing'
+$TESTING = true
 
-RSpec.configure do |c|
-  c.mock_with :rspec
-  c.color_enabled = true
-  c.tty = true
+require "pry"
+require "emque-producing"
+
+ENV["EMQUE_ENV"] = "test"
+
+module VerifyAndResetHelpers
+  def verify(object)
+    RSpec::Mocks.proxy_for(object).verify
+  end
+
+  def reset(object)
+    RSpec::Mocks.proxy_for(object).reset
+  end
+end
+
+RSpec.configure do |config|
+  config.order = "random"
+
+  config.include VerifyAndResetHelpers
 end
