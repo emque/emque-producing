581f9cfda1c61e656b27811a6e46e00804fd431f
Ryan Williams <ryan.williams@teamsnap.com>
2014-09-25 15:56:32 +0000

    Fixes #6 providing clients the ability to specify producer options
    Also making kafka options more clear by prefixing, allowing for
    other producing methods.

--- lib/emque/producing/configuration.rb
+++ lib/emque/producing/configuration.rb
@@ -2,15 +2,17 @@ module Emque
   module Producing
     class Configuration
       attr_accessor :app_name
-      attr_accessor :seed_brokers
       attr_accessor :error_handlers
       attr_accessor :log_publish_message
+      attr_accessor :kafka_seed_brokers
+      attr_accessor :kafka_producer_options
 
       def initialize
         @app_name = ""
-        @seed_brokers = ["localhost:9092"]
         @error_handlers = []
         @log_publish_message = false
+        @kafka_seed_brokers = ["localhost:9092"]
+        @kafka_producer_options = {}
       end
     end
   end
