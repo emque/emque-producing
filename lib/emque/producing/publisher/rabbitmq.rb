d791f93ef9b1156355b174b1bd71441e9f5a468b
Ryan Williams <ryan.williams@teamsnap.com>
2014-10-03 23:15:24 +0000

    Fixes #14 Close channel if exception occurs

--- lib/emque/producing/publisher/rabbitmq.rb
+++ lib/emque/producing/publisher/rabbitmq.rb
@@ -11,34 +11,37 @@ module Emque
 
         def publish(topic, message_type, message, key = nil)
           ch = @conn.create_channel
-          exchange = ch.fanout(topic, :durable => true, :auto_delete => false)
+          begin
+            exchange = ch.fanout(topic, :durable => true, :auto_delete => false)
 
-          # Assumes all messages are mandatory in order to let callers know if
-          # the message was not sent. Uses publisher confirms to wait.
-          ch.confirm_select
-          sent = true
-          exchange.on_return do |return_info, properties, content|
-            sent = false
-          end
-
-          exchange.publish(
-            message,
-            :mandatory => true,
-            :persistent => true,
-            :type => message_type,
-            :app_id => Emque::Producing.configuration.app_name,
-            :content_type => "application/json")
-
-          success = ch.wait_for_confirms
-          unless success
-            Emque::Producing.logger.warn("RabbitMQ Publisher: message was nacked")
-            ch.nacked_set.each do |n|
-              Emque::Producing.logger.warn("message id: #{n}")
+            # Assumes all messages are mandatory in order to let callers know if
+            # the message was not sent. Uses publisher confirms to wait.
+            ch.confirm_select
+            sent = true
+            exchange.on_return do |return_info, properties, content|
+              sent = false
             end
-          end
 
-          ch.close
-          return sent
+            exchange.publish(
+              message,
+              :mandatory => true,
+              :persistent => true,
+              :type => message_type,
+              :app_id => Emque::Producing.configuration.app_name,
+              :content_type => "application/json")
+
+            success = ch.wait_for_confirms
+            unless success
+              Emque::Producing.logger.warn("RabbitMQ Publisher: message was nacked")
+              ch.nacked_set.each do |n|
+                Emque::Producing.logger.warn("message id: #{n}")
+              end
+            end
+
+            return sent
+          ensure
+            ch.close unless ch.nil? || ch.closed?
+          end
         end
       end
     end
